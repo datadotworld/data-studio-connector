153b414f3c9564a806ea6da2c06be11b61bc3863
Akshaya M <69027795+saltshop-boy@users.noreply.github.com>
2021-10-04 09:56:19 +0530

    Fix for bug PAL-67. (#14)
    * Fix for bug PAL-67.Used lock service instead of caching
    
    * Error msg shown if lock is failed acquire

--- src/rest.js
+++ src/rest.js
@@ -14,53 +14,55 @@
  * limitations under the License.
  */
 
-var cachedResponseLastRefresh = null;
-var cachedResponse = null;
-
 function sql(datasetKey, sqlQuery, lastRefresh, sampleExtraction) {
-    if (!(lastRefresh && cachedResponseLastRefresh) ||
-        (lastRefresh > cachedResponseLastRefresh)) {
+    var params = {
+        method: 'post',
+        headers: {
+            'Accept': 'application/x-ndjson',
+            'Authorization': 'Bearer ' + getOAuthService().getAccessToken()
+        },
+        payload: {
+            'query': sqlQuery
+        },
+        muteHttpExceptions: true
+    };
 
-        var params = {
-            method: 'post',
-            headers: {
-                'Accept': 'application/x-ndjson',
-                'Authorization': 'Bearer ' + getOAuthService().getAccessToken()
-            },
-            payload: {
-                'query': sqlQuery
-            },
-            muteHttpExceptions: true
-        };
+    var lock = LockService.getUserLock();
 
-        var response = UrlFetchApp.fetch(getSqlEndpoint(datasetKey), params);
-        if (response.getResponseCode() === 200) {
-            cachedResponse = response;
-            cachedResponseLastRefresh = lastRefresh;
-        } else {
-            if (response.getResponseCode() === 401) {
-                resetAuth();
-            }
-
-            var errorResponse = null;
-            try {
-                errorResponse = JSON.parse(response.getContentText());
-            } catch (e) {
-                errorResponse = {code: response.getResponseCode()};
-            }
-            var errorObj = new Error(errorResponse.message || errorResponse.code);
-            logConnectorError(errorObj, "Unable to execute query. Dataset: " + datasetKey +
-                ". Query: " + sqlQuery + ". Auth valid status: " + isAuthValid() + ".");
-            throwConnectorError("Unable to execute query (error: " + errorObj.message + "). " +
-                "Please check your connection and verify that query is correct.", true);
-        }
+    try {
+        lock.waitLock(180000); //3 minutes lock timeout
+    } catch (e) {
+        var errorObj = new Error(e.message);
+        logConnectorError(errorObj, "Could not obtain lock after 3 minutes. Dataset: " + datasetKey +
+            ". Query: " + sqlQuery + ". Auth valid status: " + isAuthValid() + ".");
+        throwConnectorError("Server is too busy, please refresh after some time", true);
     }
-
-    if (sampleExtraction) {
-        // Schema extraction includes 1 schema row + 10 rows of data
-        return parseNdJson(cachedResponse.getContentText(), 11);
+    var response = UrlFetchApp.fetch(getSqlEndpoint(datasetKey), params);
+    lock.releaseLock();
+     
+    if (response.getResponseCode() === 200) {
+        if (sampleExtraction) {
+            // Schema extraction includes 1 schema row + 10 rows of data
+            return parseNdJson(response.getContentText(), 11);
+        } else {
+            return parseNdJson(response.getContentText());
+        }
     } else {
-        return parseNdJson(cachedResponse.getContentText());
+        if (response.getResponseCode() === 401) {
+            resetAuth();
+        }
+
+        var errorResponse = null;
+        try {
+            errorResponse = JSON.parse(response.getContentText());
+        } catch (e) {
+            errorResponse = {code: response.getResponseCode()};
+        }
+        var errorObj = new Error(errorResponse.message || errorResponse.code);
+        logConnectorError(errorObj, "Unable to execute query. Dataset: " + datasetKey +
+            ". Query: " + sqlQuery + ". Auth valid status: " + isAuthValid() + ".");
+        throwConnectorError("Unable to execute query (error: " + errorObj.message + "). " +
+            "Please check your connection and verify that query is correct.", true);
     }
 }
 
